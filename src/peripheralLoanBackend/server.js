30367dc0909be73eb91057b178ad6f9e7fa8110b
Prikense <a01251446@itesm.mx>
2022-04-01 23:47:35 -0600

    conexion exitosa c:

--- src/peripheralLoanBackend/server.js
+++ src/peripheralLoanBackend/server.js
@@ -1,10 +1,9 @@
-const doten = require("dotenv")
-const express = require("express")
+require("dotenv").config()
+const express = require("express");
 const ibmdb = require("ibm_db");
 
-const app = express()
+const app = express();
 
-doten.config()
 
 const port = process.env.PORT
 const host = process.env.DB_HOST
@@ -13,25 +12,30 @@ const password = process.env.DB_PASSWORD
 const dbname = process.env.DB_DATABASE
 const dbport = process.env.DB_PORT
 
+
 app.listen(port, 
     ()=> console.log(`Server Started on port ${port}...`))
 
 
 
-let cn = "DATABASE="+dbname+";HOSTNAME="+host+";PORT="+dbport+";PROTOCOL=TCPIP;UID="+user+";PWD="+password+";Security=SSL;SSLServerCertificate=ssl_certificate.arm;";
+let cn = "DATABASE="+dbname+";HOSTNAME="+host+";PORT="+dbport+";PROTOCOL=TCPIP;UID="+user+";PWD="+password+";Security=SSL;SSLServerCertificate=DigiCertGlobalRootCA.arm;";
+
 
 ibmdb.open(cn, function (err,conn) {
     console.log("querying")
     if (err){
         //return response.json({success:-1, message:err});
+        console.log("1")
         console.log(err)
     }
-    conn.query("SELECT * FROM user;", function (err, data) {
+    conn.query("SELECT * FROM LNN87839.USER;", function (err, data) {
         if (err){
             //return response.json({success:-2, message:err});
+            console.log("2")
             console.log(err)
         }
         else{
+            console.log("3")
             console.log(data)
       }
     })
