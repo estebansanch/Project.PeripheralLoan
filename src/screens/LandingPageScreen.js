9471c03373cd1f8f7ce9828f992e4ee867c74b9f
Esteban Sanchez G <a012514440@itesm.mx>
2022-04-04 19:45:20 -0500

    Create Headers, Added Headers

--- src/screens/LandingPageScreen.js
+++ src/screens/LandingPageScreen.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Button, Content } from 'carbon-components-react';
+import LandingPageHeader from './components/LandingPageHeader/';
 
 export default function LandingPageScreen() {
   return (
-    <div>
-        <h1>Si entre a la main page</h1>
-    </div>
-  )
-}
+      <>
+        <LandingPageHeader/>
+        <Content>
+          <div>
+          <h1>Si entre a la main page</h1>
+          <Button>Button</Button>
+          </div>
+        </Content>
+      </>
+    );
+};
