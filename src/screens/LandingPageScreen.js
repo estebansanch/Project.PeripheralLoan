be2e9fd109da3fa6f0573588208866ff05b659b2
Esteban Sanchez G <a012514440@itesm.mx>
2022-04-25 18:21:06 -0500

    Formatting

--- src/screens/LandingPageScreen.js
+++ src/screens/LandingPageScreen.js
@@ -22,59 +22,68 @@ export default function LandingPageScreen() {
   return (
       <>
         <LandingPageHeader/>
-          <div className="cds--row landing-page__banner">
-          <div className="cds--col-lg-16">
-            <Breadcrumb noTrailingSlash>
-              <BreadcrumbItem>
-                <a href="/">Learning the Basics</a>
-              </BreadcrumbItem>
-            </Breadcrumb>
-            <h1 className="landing-page__heading">Hardware at a few clicks away</h1>
+        <div className="cds--grid cds--grid--full-width landing-page">
+            <div className="cds--row landing-page__banner">
+            <div className="cds--col-lg-16">
+              <Breadcrumb noTrailingSlash>
+                <BreadcrumbItem>
+                  <a href="/">Learning the Basics</a>
+                </BreadcrumbItem>
+              </Breadcrumb>
+              <h1 className="landing-page__heading">Hardware at a few clicks away</h1>
+              <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
+                <div className="cds--col-lg-8 lp_heading3">
+                  Test
+                </div>
+                <div className="cds--col-lg-8 lp_heading3">Test 2
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="cds--row landing-page__r2">
-          <h2 className="landing-page__r2__heading">Powered by IBM Technology</h2>
-          <div className="cds--col cds--no-gutter">
-            <Tabs {...props.tabs}>
-              <Tab {...props.tab} label="IBM Cloud Foundry">
-                <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
-                  <div className="cds--row landing-page__tab-content">
-                    <div className="cds--col-lg-16">
-
+          <div className="cds--row landing-page__r2">
+            <h2 className="landing-page__r2__heading">Powered by IBM Technology</h2>
+            <div className="cds--col cds--no-gutter">
+              <Tabs {...props.tabs}>
+                <Tab {...props.tab} label="IBM Cloud Foundry">
+                  <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
+                    <div className="cds--row landing-page__tab-content">
+                      <div className="cds--col-lg-16">
+                        Service hosted via IBM's Cloud Foundry.
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Tab>
-              <Tab {...props.tab} label="IBM DB2 Cloud">
-                <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
-                  <div className="cds--row landing-page__tab-content">
-                    <div className="cds--col-lg-16">
-                      All data is safe behind IBMs Proprietary cloud based Database. Behind state of the 
-                      arch technology, you can rest assured that all info is secured and out of harms reach.
+                </Tab>
+                <Tab {...props.tab} label="IBM DB2 Cloud">
+                  <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
+                    <div className="cds--row landing-page__tab-content">
+                      <div className="cds--col-lg-16">
+                        All data is safe behind IBMs Proprietary cloud based Database. Behind state of the 
+                        arch technology, you can rest assured that all info is secured and out of harms reach.
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Tab>
-              <Tab {...props.tab} label="Carbon Design Systems">
-                <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
-                  <div className="cds--row landing-page__tab-content">
-                    <div className="cds--col-lg-16">
-                      Carbon provides styles and components in Vanilla, React, Angular,
-                      and Vue for anyone building on the web.
+                </Tab>
+                <Tab {...props.tab} label="Carbon Design Systems">
+                  <div className="cds--grid cds--grid--no-gutter cds--grid--full-width">
+                    <div className="cds--row landing-page__tab-content">
+                      <div className="cds--col-lg-16">
+                        Carbon provides styles and components in Vanilla, React, Angular,
+                        and Vue for anyone building on the web.
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Tab>
-            </Tabs>
+                </Tab>
+              </Tabs>
+            </div>
           </div>
-        </div>
-        <div className="cds--row landing-page__r3">
-          <div className="cds--col-md-4 cds--col-lg-4">
-            <h3 className="landing-page__label">The Principles</h3>
+          <div className="cds--row landing-page__r3">
+            <div className="cds--col-md-4 cds--col-lg-4">
+              <h3 className="landing-page__label">Resources</h3>
+            </div>
+            <div className="cds--col-md-4 cds--col-lg-4">Help Center</div>
+            <div className="cds--col-md-4 cds--col-lg-4">Contact Us</div>
+            <div className="cds--col-md-4 cds--col-lg-4">Become </div>
           </div>
-          <div className="cds--col-md-4 cds--col-lg-4">Carbon is Open</div>
-          <div className="cds--col-md-4 cds--col-lg-4">Carbon is Modular</div>
-          <div className="cds--col-md-4 cds--col-lg-4">Carbon is Consistent</div>
         </div>
       </>
     );
