e986ee9b03e2a868dd70bdadb1f1fd398c48cbb3
Esteban Sanchez Garcia <a01251440@itesm.mx>
2022-06-02 17:36:57 -0500

    d

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ const Routing = () => {
       <Route path="/peripheralAdd" element={<ProtectedRoute />}>
         <Route path="/peripheralAdd" element={<AddPeripheralScreen />}/>
       </Route>
+      {(role !== )}
       <Route path="/info" element={<ProtectedRoute />}>
         <Route path="/info" element={<PeriInfoScreen />} />
       </Route>
